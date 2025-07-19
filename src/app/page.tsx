9b9e559789f6ab5be3d20304e67f51948d37f464
Stephen Kirk <kirk11792@gmail.com>
2025-07-19 07:42:28 +0000

    Leave this in the background but bring back the image cnavas and editor

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
-import { LampDemo } from "@/components/ui/lamp";
+import StickerCustomizer from "@/components/sticker-customizer";
+import { LampContainer } from "@/components/ui/lamp";
 
 export default function Home() {
   return (
-    <LampDemo />
+    <LampContainer>
+      <div className="w-[calc(100vw-2rem)] md:w-[calc(100vw-4rem)] lg:w-[1230px] z-10 -mt-80">
+        <StickerCustomizer />
+      </div>
+    </LampContainer>
   );
 }
